4e976406edaf39340e430a324eb29c7b0978a72f
saralee-t <161432870+saralee-t@users.noreply.github.com>
2024-11-02 23:29:55 -0700

    fixed comments

--- Public/joinWindow.js
+++ Public/joinWindow.js
@@ -1,21 +1,20 @@
-// Get modal element
 const joinWindow = document.getElementById('joinWindow');
 const joinLobbyBtn = document.getElementById('joinLobbyBtn');
 const closeWindow = document.getElementsByClassName('close')[0];
 const submit = document.getElementById('submit');
 const codeInput = document.getElementById('lobbyCode');
 
-// Show the modal when Join Lobby button is clicked
+// Show the window when Join Lobby button is clicked
 joinLobbyBtn.onclick = function() {
     joinWindow.style.display = 'block';
 }
 
-// Close the modal when the 'x' is clicked
+// Close the window when the 'x' is clicked
 closeWindow.onclick = function() {
     joinWindow.style.display = 'none';
 }
 
-// Close the modal when clicking outside of the modal
+// Close the window when clicking outside of the modal
 window.onclick = function(event) {
     if (event.target == joinWindow) {
         joinWindow.style.display = 'none';
@@ -26,6 +25,5 @@ window.onclick = function(event) {
 submit.onclick = function() {
     const lobbyCode = codeInput.value;
     console.log('Lobby Code:', lobbyCode);
-    joinWindow.style.display = 'none'; // Close the modal
-    // Add further logic here to handle joining the lobby with the lobby code
+    joinWindow.style.display = 'none'; // Close the window
 }
